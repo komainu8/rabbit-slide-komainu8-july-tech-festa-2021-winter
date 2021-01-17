eb497569271ee986fa4a56d15a0857617f06175e
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-01-18 00:23:53 +0900

    Add benchmark for PGroonga's LIKE

---
+++ benchmark/address-pgroonga.sql
@@ -0,0 +1,27 @@
+CREATE TABLE zenkoku (
+    "住所CD" text,
+    "都道府県CD" text,
+    "市区町村CD" text,
+    "町域CD" text,
+    "郵便番号" text,
+    "事業所フラグ" text,
+    "廃止フラグ" text,
+    "都道府県" text,
+    "都道府県カナ" text,
+    "市区町村" text,
+    "市区町村カナ" text,
+    "町域" text,
+    "町域カナ" text,
+    "町域補足" text,
+    "京都通り名" text,
+    "字丁目" text,
+    "字丁目カナ" text,
+    "補足" text,
+    "事業所名" text,
+    "事業所名カナ" text,
+    "事業所住所" text,
+    "新住所CD" text);
+\COPY zenkoku from benchmark/date/zenkoku.csv with csv header encoding 'sjis'
+SELECT count(*) FROM zenkoku;
+CREATE INDEX zenkoku_idx ON zenkoku USING pgroonga ("市区町村", "町域");
+EXPLAIN ANALYZE SELECT "都道府県" || "市区町村" || "町域" 住所 FROM zenkoku WHERE "市区町村" LIKE '%青%' AND "町域" LIKE '%赤%';
--- exec-benchmark.sh
+++ exec-benchmark.sh
@@ -11,11 +11,21 @@ sed s/\"/\"\"/g jawiki-latest-all-titles | sed s/^/\"/g | sed s/$/\"/g > jawiki-
 popd
 
 sudo docker-compose up -d
+sleep 3
 psql -h localhost -U PGroonga --command 'CREATE DATABASE benchmark'
 psql -h localhost -U PGroonga -d benchmark -f benchmark/aozora.sql
 psql -h localhost -U PGroonga -d benchmark -f benchmark/address.sql
 psql -h localhost -U PGroonga -d benchmark -f benchmark/jawikipedia.sql
-
+sudo docker-compose down
+sleep 3
+sudo docker-compose up -d
+sleep 3
+psql -h localhost -U PGroonga --command 'CREATE DATABASE benchmark_pgroonga'
+psql -h localhost -U PGroonga -d benchmark_pgroonga --command 'CREATE EXTENSION pgroonga'
+psql -h localhost -U PGroonga -d benchmark_pgroonga -f benchmark/aozora-pgroonga.sql
+psql -h localhost -U PGroonga -d benchmark_pgroonga -f benchmark/address-pgroonga.sql
+psql -h localhost -U PGroonga -d benchmark_pgroonga -f benchmark/jawikipedia-pgroonga.sql
+sudo docker-compose down
 
 
 
